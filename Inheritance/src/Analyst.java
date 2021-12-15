b3004e62ddd37b3433321264d33e427656777978
Thoybur Rohman <73062879+Thoybur-Rohman@users.noreply.github.com>
2021-12-15 04:56:19 +0000

    inheritance
    Using inheritance to reduce code duplication

---
+++ Inheritance/src/Analyst.java
@@ -0,0 +1,8 @@
+public class Analyst extends Employee {
+
+
+    public Analyst(String name, double salary, int age) {
+        super(name,salary,age);
+    }
+
+}
---
+++ Inheritance/src/Employee.java
@@ -0,0 +1,18 @@
+public class Employee extends person {
+    protected double salary;
+
+
+    public Employee(String name, double salary, int age){
+        super(name,age);
+        this.salary = salary;
+
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void raiseSalary() {
+        this.salary = this.salary * 1.2;
+    }
+}
